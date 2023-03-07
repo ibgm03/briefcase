08f7facc921a4a0c597b0aa72e180f1e348f37e7
Benjamin Glez <ibgm03@gmail.com>
2023-03-07 10:00:26 -0600

    new navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
 import { About } from "./components/About";
-import { Barra } from "./components/Barra";
 import { Card } from "./components/Card";
 import { Contacto } from "./components/Contacto";
 import { Experience } from "./components/Experience";
 import { Footer } from "./components/Footer";
+
+import Navbar from "./components/Navbar";
 import { Skills } from "./components/Skills";
 
 function App() {
   return (
     <>
-      <Barra />
+    <Navbar/>
       <Card />
       <About />
       <Skills/>
