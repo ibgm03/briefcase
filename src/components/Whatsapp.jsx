2cc1b7f880dd5d463308cec5b38dd8551f19d807
Benjamin Glez <ibgm03@gmail.com>
2023-03-09 14:18:08 -0600

    Fix button

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -8,7 +8,7 @@ export const Whatsapp = () => {
     return (
         <div>
             <a href="https://wa.me/+524434106239/?Hola" target="_blank"
-                className='pt-1 pl-1 rounded-full fixed w-[60px] text-5xl h-[60px] z-50 leading-10 text-center text-[#fff] bg-[#25d366] bottom-[25px] right-[25px] hover:bg-[#20ba5a] ' target="_blank">
+                className='pt-1 pl-1 rounded-full fixed w-[60px] text-5xl h-[60px] z-50 leading-10 text-center text-[#fff] bg-[#25d366] bottom-[25px] right-[25px] hover:bg-[#20ba5a] '>
                 <FontAwesomeIcon icon={faWhatsapp}></FontAwesomeIcon>
             </a>
         </div>
